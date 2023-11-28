ebdb405015a7e7e9a7d5f966d6f357418e95d36a
Phil Light <139277133+plight-cmu@users.noreply.github.com>
2023-11-28 00:34:11 -0500

    Starter code for students

---
+++ main.py
@@ -0,0 +1,85 @@
+from distance_api import DistanceAPI
+from receipt_classes import Receipt, Project, Vendor
+import csv
+
+
+def main():
+    """
+    Load the projects and receipts from their input files,
+    then try to determine which project each receipt came from.
+
+    Output the results to a new CSV file which includes all the details
+    for each receipt, and adds a new column revealing which project
+    the receipt is likely associated with.
+
+    You will want to consider location and date of the transaction
+    in order to make your guess.
+    """
+    projects = load_projects_from_csv()
+    print_summary_of_projects(projects)
+
+    receipts = load_receipts_from_csv()
+    print_summary_of_receipts(receipts)
+
+    dapi = DistanceAPI()
+
+    #
+    # YOUR CODE HERE
+    #
+    # 1. Which project does each receipt belong to?
+    # 2. Print the results out to a CSV file.
+    #
+
+
+# noinspection PyTypeChecker
+def load_projects_from_csv() -> list[Project]:
+    """
+    open the file projects.csv to find a list of projects. Each project contains
+    a value for Client, Contract Start date, Work Start and End dates, and Address.
+    :return: a list of Projects, filled out with all info.
+    """
+    projects: list[Project] = []
+    with open("data/projects.csv", "r", encoding='utf-8-sig') as projectsFile:
+        project_reader = csv.DictReader(projectsFile)
+        for row in project_reader:
+            project = Project(row["Client"],
+                              row["Project Name"],
+                              row["Address"],
+                              row["Contract Date"],
+                              row["Work Start Date"],
+                              row["Work Completion Date"]
+                              )
+            projects.append(project)
+    return projects
+
+
+def load_receipts_from_csv() -> list[Receipt]:
+    """
+
+    :return:
+    """
+    # you should remove 'pass' and write code here.
+    pass
+
+
+def print_summary_of_projects(projects: list[Project]) -> None:
+    print("Projects:")
+    for project in projects:
+        print(f"• Started on {project.work_start_date}: {project}. ")
+
+
+def print_summary_of_receipts(receipts: list[Receipt]) -> None:
+    vendors = set()
+    total_spend = 0
+    for receipt in receipts:
+        vendors.add(receipt.vendor)
+        total_spend += receipt.amount
+
+    print(f"There are {len(receipts)} receipts "
+          f"from {len(vendors)} unique vendors. \n"
+          f"Total spend: ${total_spend:.2f}. "
+          f"Average receipt amount: ${total_spend/len(receipts):.2f}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ receipt_classes.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+
+class Project:
+    def __init__(self,
+                 client: str,
+                 project_name: str,
+                 address: str,
+                 contract_date: datetime,
+                 work_start_date: datetime,
+                 work_completion_date: datetime):
+        """
+
+        :rtype: object
+        """
+        self.client = client
+        self.project_name = project_name
+        self.address = address
+        self.contract_date = contract_date
+        self.work_start_date = work_start_date
+        self.work_completion_date = work_completion_date
+
+    def __str__(self) -> str:
+        return (f"{self.project_name} "
+                f"for {self.client} "
+                f"at {self.address.split(',')[0]}")
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class Vendor:
+    def __init__(self,
+                 name: str,
+                 address: str,
+                 ):
+        self.name = name
+        self.address = address
+
+    def __str__(self) -> str:
+        return f"{self.name} at {self.address}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __eq__(self, other) -> bool:
+        return (self.name == other.name and
+            self.address == other.address)
+
+    def __cmp__(self, other) -> int:
+        return self.address.__cmp__(other.address)
+
+
+class Receipt:
+    def __init__(self,
+                 when: datetime,
+                 vendor: Vendor,
+                 amount: float):
+        self.when = when
+        self.vendor = vendor
+        self.amount = amount
+        self.project = None
+
+    def set_project(self, project):
+        self.project = project
+
+    def __str__(self):
+        return f"${self.amount:.2f} at {self.vendor} on {self.when}"
+
+    def __repr__(self):
+        return self.__str__()
